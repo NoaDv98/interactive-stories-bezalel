f494e03d72b5143d60efb17ab527de8cb0560ab5
NoaDv98 <100560258+NoaDv98@users.noreply.github.com>
2024-12-22 17:16:17 +0200

    [player][cooldown]

--- llm-playground/llm-playground-web/client/src/story-config.js
+++ llm-playground/llm-playground-web/client/src/story-config.js
@@ -1,22 +1,30 @@
-const name = 'Open Story';
+const name = 'Beautiful Things';
 const instructions = `
     You are an interactive fiction narrator.  
 
     The name of the story is "${name}".
 
     Story background: 
-    ...
+    The player is a guard at a supermarket, and they are infatuated with shopping carts.
+    He's looking for every opportunity to interact with them.
+    A while back he met a convenience store owner willing to buy stolen shopping carts.
+    Today the player has a chance to steal the last cart needed for the transaction.
 
     The player's goal (important!):
-    ...
+    to get away with stealing the last cart needed for the transaction
 
     Internal logic:
-    ...
+    The player should be faced with challenges while attempting to steal the cart.
+    The player should slowly realize that they are attracted to carts.
+    If the player hasn't given a prompt in a while, prompt them to take action. 
 
     Opening background:
-    ...
+    The player is approaching he end of their shift as a guard at the supermarket. they are anxious to steal the last cart needed for the transaction.
 `;
-const openingLine = `Once upon a time...`;
+const openingLine = `You are standing at the entrance of the supermarket, the sun is setting and the parking lot is almost empty.
+You look at your watch, it's almost time for you meeting with the convenience store owner. He's expecting you to bring him 15 carts. You have 14.
+You must steal the last cart needed for the transaction.`;
+
 const firstCallToAction = ``;
 
-export const storyConfig = { name, instructions, openingLine, firstCallToAction };
+export const storyConfig = {name, instructions, openingLine, firstCallToAction};
